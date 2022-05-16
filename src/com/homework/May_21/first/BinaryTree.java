105866b616b685b5d772f804603778c58a5a077c
hongxiaobin <2812811610@qq.com>
2022-05-16 11:02:22 +0800

    2812181610@qq.com

--- src/com/homework/May_21/first/BinaryTree.java
+++ src/com/homework/May_21/first/BinaryTree.java
@@ -1,5 +1,7 @@
 package com.homework.May_21.first;
 
+import java.util.Stack;
+
 /**
  * 二叉树类
  *
@@ -11,14 +13,88 @@ public class BinaryTree<T extends Comparable<T>> {
      * 根节点
      */
     private BinaryNode<T> root;
+    private int leafs;
 
     public BinaryTree() {
         this.root = null;
+        leafs = 0;
     }
 
 
     public BinaryTree(SeqList<T> preList, SeqList<T> inList) {
         this.root = buildBitree(preList, 0, preList.size() - 1, inList, 0, inList.size() - 1);
+    }
+
+    /**
+     * 计算叶子结点的个数
+     *
+     * @Param: BinaryNode<T> node 当前树的根节点
+     * @Return:
+     */
+    public void leafCount(BinaryNode<T> node) {
+        if (node != null) {
+//            判断是否为叶子结点
+            if (node.getLeft() == null && node.getRight() == null) {
+                leafs++;
+            }
+            leafCount(node.getLeft());
+            leafCount(node.getRight());
+        }
+    }
+
+    /**
+     * 求二叉树的高度
+     *
+     * @Param: BinaryNode<T> node 当前树的根结点
+     * @Return: 二叉树的高度
+     */
+    public int getHeight(BinaryNode<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(node.getLeft());
+        int rightHeight = getHeight(node.getRight());
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    /**
+     * 依次打印从根节点到指定结点node路径上所有结点，并求路径长度
+     *
+     * @Param: BinaryNode<T> node 目的结点
+     * @Return: 路径长度
+     */
+    public int printPath(BinaryNode<T> node) {
+        if (root != null) {
+            printPath(root,node);
+            System.out.println(stack);
+            return stack.size();
+        }
+        return 0;
+    }
+
+    Stack<BinaryNode<T>> stack = new Stack<>();
+
+    public boolean printPath(BinaryNode<T> root, BinaryNode<T> node) {
+        if (root == null) {
+            return false;
+        }
+//        遍历到的入栈
+        stack.push(root);
+//        找到目标结点就停止
+        if (root == node) {
+            return true;
+        }
+        if (printPath(root.getLeft(), node)) {
+//            左子树找不到，因为root == null  return false
+            return true;
+        }
+        if (printPath(root.getRight(), node)) {
+//            右子树找不到
+            return true;
+        }
+//        如果其左右子节点都找不到就出栈，表示找到尽头了找不到，故此结点不会是路径中的一点
+        stack.pop();
+        return false;
     }
 
     /**
@@ -49,10 +125,129 @@ public class BinaryTree<T extends Comparable<T>> {
         }
     }
 
+
+    /**
+     * 查找并删除首个以与key相等元素为结点为根的子树
+     *
+     * @Param: T key 要删除结点的值
+     * @Return: BinaryNode<T> 需要删除的结点
+     */
+    public BinaryNode<T> remove(T key) {
+        return remove(root, key);
+    }
+
+    public BinaryNode<T> remove(BinaryNode<T> node, T key) {
+        BinaryNode<T> temp = null;
+        if (node != null) {
+            if (node.getRight() != null && node.getRight().getData().equals(key)) {
+                temp = node.getRight();
+                node.setRight(null);
+            }
+            if (node.getLeft() != null && node.getLeft().getData().equals(key)) {
+                temp = node.getLeft();
+                node.setLeft(null);
+            }
+            remove(node.getLeft(), key);
+            remove(node.getRight(), key);
+        }
+        return temp;
+    }
+
+    /**
+     * 返回某结点的父母结点
+     *
+     * @Param: BinaryNode<T> p 需要找到其父母结点的子节点
+     * @Return: BinaryNode<T> 其父母结点
+     */
+    public BinaryNode<T> parent(BinaryNode<T> p) {
+        if (root != null) {
+            return parent(root, p);
+        }
+        return null;
+    }
+
+    public BinaryNode<T> parent(BinaryNode<T> root, BinaryNode<T> p) {
+        if (root == null || root.getLeft() == p || root.getRight() == p) {
+            return root;
+        }
+        BinaryNode<T> left = parent(root.getLeft(), p);
+        if (left != null) {
+            return left;
+        }
+        BinaryNode<T> right = parent(root.getRight(), p);
+        if (right != null) {
+            return right;
+        }
+        return null;
+    }
+
+    /**
+     * 判断是否为二叉排序树
+     *
+     * @Param:
+     * @Return: true 是二叉排序树 反之不是
+     */
+    public boolean isSorted() {
+        if (root != null) {
+            return isSorted(root);
+        }
+        return false;
+    }
+
+    public boolean isSorted(BinaryNode<T> root) {
+        boolean s = true;
+        if (root == null) {
+            return true;
+        }
+        if (root.getLeft() != null) {
+            if (root.getLeft().getData().compareTo(root.getData()) >= 0) {
+                s = false;
+            }
+        }
+        if (root.getRight() != null) {
+            if (root.getRight().getData().compareTo(root.getData()) <= 0) {
+                s = false;
+            }
+        }
+        isSorted(root.getLeft());
+        isSorted(root.getRight());
+        return s;
+    }
+
+    /**
+     * 查找指定结点
+     *
+     * @Param: T data 指定值
+     * @Return: 指定结点
+     */
+    public BinaryNode<T> getNode(T data) {
+        if (root != null) {
+            return getNode(root, data);
+        }
+        return null;
+    }
+
+    public BinaryNode<T> getNode(BinaryNode<T> root, T data) {
+        BinaryNode<T> temp = null;
+        if (root.getData().equals(data)) {
+            return root;
+        }
+        if (root.getLeft() != null) {
+            temp = getNode(root.getLeft(), data);
+        }
+        if (temp != null) {
+            return temp;
+        }
+        if (root.getRight() != null) {
+            temp = getNode(root.getRight(), data);
+        }
+        return temp;
+    }
+
     /**
      * 先序遍历二叉树
      *
-     * @Param: BinaryNode<T> node 当前结点，根节点
+     * @Param: BinaryNode<T> node 当前结点
      * @Return:
      */
     public void preOrder(BinaryNode<T> node) {
@@ -91,6 +286,14 @@ public class BinaryTree<T extends Comparable<T>> {
         }
     }
 
+    public int getLeafs() {
+        return leafs;
+    }
+
+    public void setLeafs(int leafs) {
+        this.leafs = leafs;
+    }
+
     public BinaryNode<T> getRoot() {
         return root;
     }
@@ -98,6 +301,8 @@ public class BinaryTree<T extends Comparable<T>> {
     public void setRoot(BinaryNode<T> root) {
         this.root = root;
     }
+
+
 }
 
 /**
@@ -106,7 +311,7 @@ public class BinaryTree<T extends Comparable<T>> {
  * @Author: hongxiaobin
  * @Date: 2022/5/15 15:04
  */
-class BinaryNode<T> {
+class BinaryNode<T extends Comparable<T>> {
     private T data;
     private BinaryNode<T> left;
     private BinaryNode<T> right;
--- src/com/homework/May_21/first/BinaryTreeTest.java
+++ src/com/homework/May_21/first/BinaryTreeTest.java
@@ -26,7 +26,38 @@ public class BinaryTreeTest {
         System.out.println("后序遍历");
         stringBinaryTree.postOrder(stringBinaryTree.getRoot());
         System.out.println();
-
+//        统计叶子结点数
+        System.out.println("该二叉树的叶子结点数为");
+        stringBinaryTree.leafCount(stringBinaryTree.getRoot());
+        System.out.println(stringBinaryTree.getLeafs());
+//        计算二叉树的高度
+        System.out.println("该二叉树的高度为");
+        System.out.println(stringBinaryTree.getHeight(stringBinaryTree.getRoot()));
+//        求根节点到指定结点的路径
+        System.out.println("根节点到结点 g 的路径");
+        BinaryNode<String> temp = stringBinaryTree.getNode("g");
+        System.out.println("路径长度为"+stringBinaryTree.printPath(temp));
+//        返回某结点的父母结点
+        System.out.println("结点 g 的父母结点为");
+        BinaryNode<String> g = stringBinaryTree.getNode("g");
+        System.out.println(stringBinaryTree.parent(g));
+//        删除某个结点为根节点的子树
+        System.out.println("删除以值为 e 结点为根节点的子树");
+        stringBinaryTree.remove("e");
+        stringBinaryTree.preOrder(stringBinaryTree.getRoot());
+        System.out.println();
+//        判断是否为二叉排序树
+        SeqList<Integer> preList1 = new SeqList<>();
+        SeqList<Integer> inList1 = new SeqList<>();
+        int[] pre = {7,3,1,2,5,10,9,12};
+        int[] in = {1,2,3,5,7,9,10,12};
+        for (int i = 0; i < pre.length; i++) {
+            preList1.insert(pre[i]);
+            inList1.insert(in[i]);
+        }
+        BinaryTree<Integer> integerBinaryTree =new BinaryTree<>(preList1,inList1);
+        System.out.println("该二叉树是否为二叉排序树");
+        System.out.println(integerBinaryTree.isSorted());
     }
     private static void extracted(SeqList<String> preList, String preStr) {
         for (int i = 0; i < preStr.length(); i++) {
